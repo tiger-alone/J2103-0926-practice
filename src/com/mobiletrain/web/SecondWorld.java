8b7e78feae66b3278f04a7eeadc079f548973f9b
tiger-alone <2115497077@qq.com>
2021-09-26 13:52:30 +0800

    等等

---
+++ src/com/mobiletrain/web/SecondWorld.java
@@ -0,0 +1,11 @@
+package com.mobiletrain.web;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+        System.out.println("嘻嘻");
+        System.out.println("天道酬勤笃学奋进");
+
+	
+    }
+}
---
+++ src/com/mobiletrain/web/ThirdWorld.java
@@ -0,0 +1,9 @@
+package com.mobiletrain.web;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+        System.out.println("嘻嘻");
+        System.out.println("他日若遂凌云志，敢笑黄巢不丈夫");
+    }
+}
